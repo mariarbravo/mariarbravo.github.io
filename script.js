847a34e2e198de5ee5242b1deb3588008d9af7af
mariarbravo <mariaapprb@gmail.com>
2025-08-07 12:44:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetID = this.getAttribute('href');
+    const targetElement = document.querySelector(targetID);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
 
+console.log("Welcome to Maria Bravo's Portfolio Site ✨");
